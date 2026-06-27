09a243aea76447bbb0cb4cc01a0e259ae42b0d53
Zang <jordan.de@orange.fr>
2026-06-27 18:33:02 +0200

    feat: add distance API

---
+++ Engine/Include/GeoCore/Math/Distance.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <GeoCore/Position.hpp>
+
+namespace GeoCore::Math
+{
+    double distance(const Position& a, const Position& b);
+}
---
+++ Engine/Source/Math/Distance.cpp
@@ -0,0 +1,14 @@
+#include <GeoCore/Math/Distance.hpp>
+
+namespace GeoCore::Math
+{
+
+double distance(const Position& a, const Position& b)
+{
+    (void)a;
+    (void)b;
+
+    return 0.0;
+}
+
+}
