27ecfcdefd6c833bf63e80b248d7ed4228033731
Zang <jordan.de@orange.fr>
2026-06-27 18:35:54 +0200

    feat: implement haversine distance

--- Engine/Source/Math/Distance.cpp
+++ Engine/Source/Math/Distance.cpp
@@ -1,14 +1,43 @@
 #include <GeoCore/Math/Distance.hpp>
 
+#include <cmath>
+
+namespace
+{
+    constexpr double EarthRadius = 6371000.0; // meters
+
+    constexpr double Pi = 3.14159265358979323846;
+
+    double toRadians(double degrees)
+    {
+        return degrees * Pi / 180.0;
+    }
+}
+
 namespace GeoCore::Math
 {
 
 double distance(const Position& a, const Position& b)
 {
-    (void)a;
-    (void)b;
+    const double latitude1 = toRadians(a.latitude());
+    const double longitude1 = toRadians(a.longitude());
 
-    return 0.0;
+    const double latitude2 = toRadians(b.latitude());
+    const double longitude2 = toRadians(b.longitude());
+
+    const double deltaLatitude = latitude2 - latitude1;
+    const double deltaLongitude = longitude2 - longitude1;
+
+    const double haversine =
+        std::sin(deltaLatitude / 2.0) * std::sin(deltaLatitude / 2.0) +
+        std::cos(latitude1) * std::cos(latitude2) *
+        std::sin(deltaLongitude / 2.0) * std::sin(deltaLongitude / 2.0);
+
+    const double centralAngle =
+        2.0 * std::atan2(std::sqrt(haversine),
+                         std::sqrt(1.0 - haversine));
+
+    return EarthRadius * centralAngle;
 }
 
 }
